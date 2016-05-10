555e324e63e2184b0b9b41f9e3beb76bc2b5ab7a
marko <namahshivaya99@gmail.com>
2016-05-10 19:51:34 +0200

    initial commit

---
+++ DataAccessLayer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DataAccessLayer
+    {
+        private string _connectionString = string.Empty;
+        private SqlConnection _sqlConnection = null;
+
+        public DataAccessLayer(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public QueryResult ExecuteProcedure(string procedureName, List<SqlParameter> parameters)
+        {
+            QueryResult queryResult = new QueryResult();
+
+            try
+            {
+                if (_sqlConnection == null)
+                     _sqlConnection = new SqlConnection(_connectionString);
+
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter())
+                {
+                    dataAdapter.SelectCommand = new SqlCommand(procedureName, _sqlConnection);
+                    dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                    // adding all parameters for the procedure call
+                    for (int i = 0; i < parameters.Count; i++)
+                    {
+                        dataAdapter.SelectCommand.Parameters.Add(parameters[i]);
+                    }
+
+                    // procedure call and saving results
+                    DataSet dataSet = new DataSet();
+                    dataAdapter.Fill(dataSet, "Result");
+                    queryResult._resultTable = dataSet.Tables["Result"];
+                }
+            }
+            catch (SqlException ex)
+            {
+                queryResult._errorMessage = ex.Message;
+            }
+            catch (Exception ex)
+            {
+                queryResult._errorMessage = ex.Message;
+            }
+            return queryResult;
+        }
+    }
+}
---
+++ QueryResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class QueryResult
+    {
+        internal DataTable _resultTable = new DataTable();
+        internal string _errorMessage = string.Empty;
+
+        public DataTable ResultTable
+        {
+            get
+            {
+                return _resultTable;
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get
+            {
+                return _errorMessage;
+            }
+        }
+
+        public QueryResult(DataTable resultTable, string errorMessage)
+        {
+            _resultTable = resultTable;
+            _errorMessage = errorMessage;
+        }
+
+        public QueryResult() { }
+
+        public bool HasError() { return _errorMessage != string.Empty; }
+        public bool HasResult() { return _resultTable.Rows.Count > 0; }
+
+        public object GetResult(int rowNumber, string columnName)
+        {
+            return ResultTable.Rows[rowNumber][columnName];
+        }
+    }
+}
